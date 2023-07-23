fe5019667ac07289125a3721e37c679dec88ab3b
KrozhDev <roko.mdn@gmail.com>
2023-07-23 08:53:48 +0300

    first commit

---
+++ src/main/java/pro/sky/quest_gen/QuestGenApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.quest_gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestGenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/quest_gen/QuestGenApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.quest_gen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
